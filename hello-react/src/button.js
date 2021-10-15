1ceb9432f7616748fdb943429ed74f8f8b8c727b
DESKTOP-UQU9FHO\ryoka <ryo.kamiya.1988@gmail.com>
2021-10-16 01:03:49 +0900

    React勉強 #4 https://www.youtube.com/watch?v=mjrNtEIk5Fs

---
+++ hello-react/src/button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Button =()=>{
+  return (
+    <button className="ui basic button">
+      <i className="icon user"></i>
+      Add Friend
+    </button>
+  )
+}
+
+export default Button;
